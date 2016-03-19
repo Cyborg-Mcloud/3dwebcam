552adb368e6d85bd831570614927b19507066e02
Cyborg-Mcloud <uabashidze@gmail.com>
2016-03-20 00:53:11 +0400

    er

--- metawear.js
+++ metawear.js
@@ -63,7 +63,7 @@ var metawear = {
     subscribeForIncomingData: function() {
         console.log(arguments);        
       //  ble.startNotification(metawear.deviceId, metawear.serviceUUID, metawear.rxCharacteristic, metawear.onDataReceived, metawear.onDataReceivedError);
-		ble.startNotification(metawear.deviceId, "2902", "2A01", metawear.onDataReceived, metawear.onDataReceivedError);
+		ble.startNotification(metawear.deviceId, "180f", "2A19", metawear.onDataReceived, metawear.onDataReceivedError);
     },
     accLOCK : false,
     
