b06d9b0531495e78bf9321473fad7c52da9ada32
Cyborg-Mcloud <uabashidze@gmail.com>
2016-03-23 10:12:44 +0400

    reer
    r

--- metawear.js
+++ metawear.js
@@ -338,8 +338,8 @@ mz:0
 		var data = new Uint8Array(4);
 		data[0] = 0x03;
 		data[1] = 0x03;
-		data[2] = 0x24;  // 0x20 + herz
-		data[3] = 0x03;  // G Sens
+		data[2] = 0x27;  // 0x20 + herz
+		data[3] = 0x04;  // G Sens
         metawear.writeData(data.buffer); // setting 50hz and 2G
 
         var data = new Uint8Array(3);
